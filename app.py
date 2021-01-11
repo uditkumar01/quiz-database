add270fc86c4b19b61e69a2d4232fb707c73f367
Udit Kumar <55291327+uditkumar01@users.noreply.github.com>
2021-01-12 01:25:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,14 @@ def save_user():
         user = User(name = data.get('name'),points = data.get('points'), user_type = data.get('user_type'))
         db.session.add(user)
         db.session.commit()
-        all_users = User.query.order_by(User.points.desc()).filter_by(user_type = data.get('user_type')).limit(5).all()
+        all_users = User.query.order_by(User.points.desc()).filter_by(user_type = data.get('user_type')).all()
         users,rank,count = [],-1,0
         for user1 in all_users:
             count+=1
             if rank==-1 and user1.name == data.get('name') and user1.points == data.get('points') and user1.user_type == data.get('user_type'):
                 rank = count
-            users.append({"name":user1.name,"points":user1.points,"user_type":user1.user_type})
+            if count<=10:
+                users.append({"name":user1.name,"points":user1.points,"user_type":user1.user_type})
             
         data = {'data':users,'rank':rank}
         response = app.response_class(
